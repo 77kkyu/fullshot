aed2ef06e1dc615ea7a68d88a439ad9d2c8a89cd
CheulKyu Yoon <77kkyu@gmail.com>
2021-01-07 18:28:39 +0900

    Add files via upload

---
+++ src/main/java/com/example/fullshot/FullshotApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullshotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullshotApplication.class, args);
+    }
+
+}
