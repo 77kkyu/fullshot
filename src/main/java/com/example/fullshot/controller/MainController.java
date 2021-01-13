65b2a9bef26ae53316957d8783ad0fb33e7a9f03
CheulKyu Yoon <77kkyu@gmail.com>
2021-01-13 20:57:54 +0900

    타임리프 기본 문법
    타임리프 기본 문법

--- src/main/java/com/example/fullshot/controller/MainController.java
+++ src/main/java/com/example/fullshot/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.example.fullshot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/test")
-    public String test() {
+    public String test(Model model) {
+        /* 테스트 완료 */
+        model.addAttribute("msg", "1000");
         return "home";
     }
 }
