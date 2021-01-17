09ce053ac4271a9f22b1d32fc39dff9e3723ea0a
CheulKyu Yoon <77kkyu@gmail.com>
2021-01-17 22:10:38 +0900

    로그인 추가
    로그인 추가

---
+++ src/main/java/com/example/fullshot/papa/login/model/Login.java
@@ -0,0 +1,12 @@
+package com.example.fullshot.papa.login.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Login {
+
+    private String userId;
+
+}
---
+++ src/main/java/com/example/fullshot/papa/login/service/LoginService.java
@@ -0,0 +1,7 @@
+package com.example.fullshot.papa.login.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+}
