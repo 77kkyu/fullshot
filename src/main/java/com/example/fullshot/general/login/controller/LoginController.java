041cad7471b5cbb9d9fa165352f1068df06f6ddd
DESKTOP-7HK7PM8\kkyu <77kkyu@gmail.com>
2021-05-17 21:46:20 +0900

    jsp 화면 템플릿 테스트

--- src/main/java/com/example/fullshot/general/login/controller/LoginController.java
+++ src/main/java/com/example/fullshot/general/login/controller/LoginController.java
@@ -1,18 +1,22 @@
 package com.example.fullshot.general.login.controller;
 
+import com.example.fullshot.general.commonBoard.model.CommonBoardDTO;
 import com.example.fullshot.general.login.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
 @Controller
 public class LoginController {
 
-    private void test(){
-        User user = new User();
+    @GetMapping("/main")
+    public String aa() {
+        return "home";
     }
 
-    
 }
