a80f04e2adb059a39313b9cf6d53c6a486a38dc3
DESKTOP-7HK7PM8\kkyu <77kkyu@gmail.com>
2021-05-24 20:18:37 +0900

    Optional test

--- src/main/java/com/example/fullshot/general/commonBoard/model/CommonBoardRepository.java
+++ src/main/java/com/example/fullshot/general/commonBoard/model/CommonBoardRepository.java
@@ -1,7 +1,9 @@
 package com.example.fullshot.general.commonBoard.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommonBoardRepository extends JpaRepository<CommonBoardDTO, String> {
 
     public CommonBoardDTO findByBoardId(String id);
